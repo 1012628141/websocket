db8db021bf891c31f02b517021a0ab332a6eebe3
junyi.zhu <zjypyj@163.com>
2018-05-09 17:56:40 +0800

    初次测试

--- src/main/java/com/example/demo/websocket/WebMvcConfig.java
+++ src/main/java/com/example/demo/websocket/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,8 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
-    @Override
+   /* @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/ws").setViewName("/ws");
+    }*/
+
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resource/**").addResourceLocations("/templates/");
+    }*/
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/ws").setViewName("/ws");
     }
 }
--- src/main/java/com/example/demo/websocket/WiselyController.java
+++ src/main/java/com/example/demo/websocket/WiselyController.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author tangmin
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WiselyController {
+    public  static int i=0;
 
     @MessageMapping("/welcome")  //浏览器向服务端发请求的地址，类似@RequestMapping
     @SendTo("/topic/getResponse") //当服务器端有消息时，会对订阅了@SendTo中的路径的浏览器发送消息
@@ -29,5 +32,14 @@ public class WiselyController {
         //Thread.sleep(1000);
         return new WiselyResponse("welcome,"  +":"+dateFormat.format(new Date())+ " !");
     }
+
+    /*@MessageMapping("/welcome")  //浏览器向服务端发请求的地址，类似@RequestMapping
+    @SendTo("/topic/getResponse") //当服务器端有消息时，会对订阅了@SendTo中的路径的浏览器发送消息
+    public int say() throws Exception{
+
+        return i++;
+    }*/
+
+
 
 }
